a264f0b95ec40c76c58df1cf70ab13799a729567
mdestiakahmed <estiak97@gmail.com>
2024-04-05 16:41:45 +0600

    Add ModalBox components to molecules index

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,3 +4,4 @@ export { default as TextInput } from "./TextInput";
 export { default as CheckBox } from "./CheckBox";
 export { default as TextArea } from "./TextArea";
 export { default as SelectBox } from "./SelectBox";
+export { default as ModalBox } from "./ModalBox";
